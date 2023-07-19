f8e946aab584433b121d0b240f2ecf63e52f7045
Isaac Kilbourne <kilbouri@uwindsor.ca>
2023-07-19 19:28:36 -0400

    Start building homepage layout

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -2,3 +2,33 @@ export interface LastUser {
   email: string;
   secretKey: string; // TODO: this really needs to be encrypted
 }
+
+export interface StoredObject {
+  uuid: string;
+}
+
+export interface Vault extends StoredObject {
+  name: string;
+  items: VaultItem[];
+}
+
+export interface VaultItem extends StoredObject {
+  name: string;
+  summaryText: string;
+  fields: Field[];
+}
+
+export interface Field {
+  label: string;
+  value: FieldValue;
+}
+
+export enum FieldType {
+  PlainText,
+}
+
+export interface FieldValue {
+  type: FieldType;
+  isMasked: boolean;
+  base64Value: string;
+}
